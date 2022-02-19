042b439cb8a6feeda32f62e63b97cb5f56795462
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2022-02-19 16:58:20 +0800

    Set API url to env variable

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: process.env.REACT_APP_BASE_API_URL,
 });
 
 const responseBody = (res) => res.data;
