34baa2db3a2d5b5b4d8c97e81ec45f138e08d749
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2022-03-22 22:39:54 +0800

    Added chart component

---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+var colors = [
+  "#FEF3D2",
+  "#EDCFC0",
+  "#C3D3D6",
+  "#D5DBE4",
+  "#E4D5E3",
+  "#AAC9CE",
+  "#B6B4C2",
+  "#C9BBC8",
+  "#E5C1CD",
+];
+
+export default colors;
--- src/pages/Dashboard/Reports/Reports.js
+++ src/pages/Dashboard/Reports/Reports.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import BarChart from "components/Charts/BarChart";
 
 const StyledH1 = styled.h1`
   padding-top: 30px;
@@ -7,10 +8,30 @@ const StyledH1 = styled.h1`
   font-weight: bold;
 `;
 
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const ChartContainer = styled.div`
+  height: 100%;
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Reports = (props) => {
   return (
     <>
       <StyledH1>Reports</StyledH1>
+      <ContentContainer>
+        <ChartContainer>
+          <BarChart />
+        </ChartContainer>
+      </ContentContainer>
     </>
   );
 };
