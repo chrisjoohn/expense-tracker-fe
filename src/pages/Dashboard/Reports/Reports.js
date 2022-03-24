7592ed050a5fe14f4183dee0e5f92c0ef9d03b4a
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2022-03-24 20:15:09 +0800

    Added initial select dropdown for sorting on reports

--- src/pages/Dashboard/Reports/Reports.js
+++ src/pages/Dashboard/Reports/Reports.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import BarChart from "components/Charts/BarChart";
+import Select from "components/Inputs/Select";
 
 const StyledH1 = styled.h1`
   padding-top: 30px;
@@ -8,30 +9,58 @@ const StyledH1 = styled.h1`
   font-weight: bold;
 `;
 
-const ContentContainer = styled.div`
+const ContentWrapper = styled.div`
   width: 100%;
-  height: 100vh;
   display: flex;
   justify-content: center;
 `;
 
-const ChartContainer = styled.div`
-  height: 100%;
+const ContentContainer = styled.div`
   width: 70%;
+  height: 100vh;
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
 `;
+
+const ChartContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 1;
+`;
+
+const SorterContainer = styled.div`
+  align-self: flex-start;
+  display: flex;
+  padding: 10px;
+  z-index: 2;
+`;
+
+const options = [
+  { value: "year", placeholder: "Year" },
+  { value: "quarter", placeholder: "Quarter" },
+  { value: "month", placeholder: "Month" },
+  { value: "week", placeholder: "Week" },
+];
 
 const Reports = (props) => {
   return (
     <>
       <StyledH1>Reports</StyledH1>
-      <ContentContainer>
-        <ChartContainer>
-          <BarChart />
-        </ChartContainer>
-      </ContentContainer>
+
+      <ContentWrapper>
+        <ContentContainer>
+          <SorterContainer>
+            <Select placeholder="Sort by: " options={options} />
+          </SorterContainer>
+          <ChartContainer>
+            <BarChart />
+          </ChartContainer>
+        </ContentContainer>
+      </ContentWrapper>
     </>
   );
 };
